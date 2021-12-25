e2da2c8b35dbe2785c37b50c676ba4b19cc29ce0
digitalvodka95 <digitalvodka95@gmail.com>
2021-12-25 02:06:23 +0100

    file system parses done

--- Assets/Scripts/FileSync.cs
+++ Assets/Scripts/FileSync.cs
@@ -14,71 +14,123 @@ public class FileSync : MonoBehaviour{
 
     public IEnumerator SyncFilesWithServer(){
         var datadir = Environment.CurrentDirectory + @"\TTData";
-        Debug.Log("data directory is = " + datadir);
-        var ftm = new FileTreeManager(datadir);
-        Debug.Log(ftm.ToString());
+        var ftm = new DirectoryManager(datadir);
+        Debug.Log(ftm.ToJson());
         
         // dispatcher.Enqueue(loginManager.LoadMainMenu());
         yield return null;
     }
 }
 
-public class FileTreeManager{
-    private DirectoryInfo mDirectoryInfo;
-    public FileTreeManager(string aEntryPoint)
+public class DirectoryManager{    
+    Directory mDir;
+
+    public DirectoryManager(string mEntryPath)
     {
-        SetEntryPoint(aEntryPoint);
+        Debug.Log("parent dir path: " + mEntryPath);
+        mDir = GetSubdirectoriesAndFilesRecursive(mEntryPath);
     }
 
-    public void SetEntryPoint(string aEntryPoint)
-    {
-        this.mDirectoryInfo = new DirectoryInfo(aEntryPoint);
-    }
+    public Directory GetSubdirectoriesAndFilesRecursive(string entrypath){
+        var thisdir = new DirectoryInfo(entrypath);
+        Directory res = new Directory(thisdir.FullName, thisdir.Name);
 
-    public Directory GetDirInfo(){
-        Directory res = new Directory(mDirectoryInfo.Name);
-        foreach (DirectoryInfo dir in this.mDirectoryInfo.EnumerateDirectories())
-        {
-            foreach (var file in dir.EnumerateFiles())
-            {
-                var fileInfo = file.Name + " - " + file.Length.ToString() + " - " + file.LastWriteTimeUtc.ToString();
-                result.Append("--|-- " + fileInfo);
-            }
+        foreach (var file in thisdir.EnumerateFiles()){
+            File f = new File(file.Name, file.FullName, file.Length, file.LastWriteTimeUtc);
+            res.mFiles.Add(f);
         }
+
+        foreach (DirectoryInfo dir in thisdir.EnumerateDirectories()){
+            var subdir = GetSubdirectoriesAndFilesRecursive(dir.FullName);
+            res.mSubdirectories.Add(subdir);
+        }
+
+        return res;
     }
 
-    public override string ToString()
-    {
-        StringBuilder result = new StringBuilder();
-        foreach (DirectoryInfo dir in this.mDirectoryInfo.EnumerateDirectories())
-        {
-            result.Append("|-- " + dir.Name + Environment.NewLine);
-            foreach (var file in dir.EnumerateFiles())
-            {
-                var fileInfo = file.Name + " - " + file.Length.ToString() + " - " + file.LastWriteTimeUtc.ToString();
-                result.Append("--|-- " + fileInfo);
+    public string ToJson(){
+        var res = "{";
+        res += JsonRecursive(mDir);
+        res += "}";
+
+        return res;
+    }
+
+    string JsonRecursive(Directory dir){
+        // string res = @"
+        // {
+        //     ---START WRITE---
+        //     ""directoryName"":{
+        //         ""files"":[
+        //             {""name"":""filename.txt"",""size"":123,""lastModified"":""12/03/24T23:45:16-GTM+03:00},
+        //             {---}
+        //         ]
+        //         --- WRITE NEXT ---
+        //         ""directoryName"":{
+        //             ""files"":[...]
+        //             ---WRITE NEXT---
+        //             ""directoryName"":{
+        //                 ""files"":[...]
+        //             }--- FINISH WRITE ---
+        //         }--- FINISH WRITE ---
+        //     }--- FINISH WRITE ---
+        // }";
+
+        string res = "";
+        
+        res += @$"""{dir.mName}"":{{
+""files"":";
+
+        string files = "[";
+        int cont = 1;
+        foreach (var file in dir.mFiles){
+            files += @$"{{""name"":""{file.path.Replace(@"\", @"\\")}"",""size"":{file.size},""lastModified"":""{file.lastModified}""}}";
+            if(dir.mFiles.Count != cont){
+                files += ",";
             }
+            cont ++;
         }
-        return result.ToString();
+        
+        res+= files + "]";
+        if(dir.mSubdirectories.Count == 0){
+            res +="\n";
+        }else{
+            res +=",\n";
+        }
+
+        foreach (var subdir in dir.mSubdirectories){
+            res += JsonRecursive(subdir);
+        }
+        res +="}\n";
+        return res;
     }
 }
 
 public class Directory{
-    public string mName;
+    public string mPath, mName;
     public List<Directory> mSubdirectories;
     public List<File> mFiles;
-    public Directory(string name){
+    public Directory(string path, string name){
         mSubdirectories = new List<Directory>();
         mFiles = new List<File>();
+        mPath = path;
         mName = name;
     }
-
 }
+
 public class File{
-    public string name;
+    public string name, path;
     public long size;
     public DateTime lastModified;
-    public File(string name, long size, DateTime lastModified){
+    public File(string name, string path, long size, DateTime lastModified){
+        this.name = name;
+        this.path = path;
+        this.size = size;
+        this.lastModified = lastModified;
+    }
 
+    public override string ToString()
+    {
+        return path + " - " + size.ToString() + " - " + lastModified.ToString();
     }
 }
